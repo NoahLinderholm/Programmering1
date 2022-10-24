47b6941d934b77acc8b278f54afe35b3eabb42f5
NoahLinderholm <112382681+NoahLinderholm@users.noreply.github.com>
2022-10-24 09:40:41 +0200

    Initial commit

---
+++ Inlämningsuppgift.cs
@@ -0,0 +1,23 @@
+﻿Random rnd = new Random();
+int svar = rnd.Next(1,101);
+int i=0;
+for(int y = 1; i!=svar; y++)
+{
+Console.WriteLine("Gissa ett tal mellan 1 och 100");
+i = Convert.ToInt32(Console.ReadLine());
+if(i > svar)
+{
+    Console.WriteLine("Du gissade för högt");
+}
+else if (i < svar)
+{
+    Console.WriteLine("Du gissade för lågt");
+}
+else
+{
+    Console.WriteLine("Du gissade rätt");
+}
+{
+    Console.WriteLine("Du har gjort "+y+" gissningar");
+}
+}
